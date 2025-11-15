b81a843dd1b3816510aa7f8a263769639ab0215b
AlexeyStatnykh <tancor.89@mail.ru>
2025-11-16 02:09:05 +0500

    Добавил очевидные модели данных

---
+++ NumberDeclension/NumberDeclension.Domain/Models/GrammaticalCase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberDeclension.Domain.Models
+{
+    public readonly struct GrammaticalCase
+    {
+        private GrammaticalCase(string code) => Code = code;
+        public string Code { get; }
+        public static readonly GrammaticalCase Nominative = new GrammaticalCase("И");
+        public static readonly GrammaticalCase Genitive = new GrammaticalCase("Р");
+        public static readonly GrammaticalCase Dative = new GrammaticalCase("Д");
+        public static readonly GrammaticalCase Accusative = new GrammaticalCase("В");
+        public static readonly GrammaticalCase Instrumental = new GrammaticalCase("Т");
+        public static readonly GrammaticalCase Prepositional = new GrammaticalCase("П");
+    }
+
+}
---
+++ NumberDeclension/NumberDeclension.Domain/Models/GrammaticalGender.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberDeclension.Domain.Models
+{
+    public readonly struct GrammaticalGender
+    {
+        private GrammaticalGender(string code) => Code = code;
+        public string Code { get; }
+        public static readonly GrammaticalGender Masculine = new GrammaticalGender("М");
+        public static readonly GrammaticalGender Feminine = new GrammaticalGender("Ж");
+        public static readonly GrammaticalGender Neuter = new GrammaticalGender("С");
+    }
+}
